8317205ac7634b963b005d513b7d772b4193b756
TanyaSoni2912 <tanu139sona@gmail.com>
2025-01-23 17:40:28 +0530

    env file access in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-// test push
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Login } from './pages/Login';
