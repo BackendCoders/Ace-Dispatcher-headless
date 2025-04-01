ca190ea29457e6b4d4139a6952a5a4f96a8b882a
TanyaSoni2912 <tanu139sona@gmail.com>
2025-04-01 17:29:21 +0530

    price fixed to two decimal place

--- src/components/CustomDialog.jsx
+++ src/components/CustomDialog.jsx
@@ -541,7 +541,7 @@ function CustomDialog({ closeDialog }) {
 											head='Allocated Driver'
 										/>
 										<BookingOption
-											text={data.price ? `£${data.price?.toFixed(2, 0)}` : 'NA'}
+											text={data.price ? `£${data.price?.toFixed(2)}` : 'NA'}
 											head='Price'
 										/>
 										<BookingOption
