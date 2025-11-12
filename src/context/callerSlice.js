b32b325ef3c4ebfeadf300e881b5ac4b325b4ff3
TanyaSoni29 <tanu139sona@gmail.com>
2025-11-12 22:08:35 +0530

    scope and account number in caller is cash and 9999

--- src/context/callerSlice.js
+++ src/context/callerSlice.js
@@ -1,84 +1,88 @@
 /** @format */
 
-import { createSlice } from '@reduxjs/toolkit';
-import { convertKeysToCamelCase } from '../utils/casingConverter';
-import { formatDate } from '../utils/formatDate';
+import { createSlice } from "@reduxjs/toolkit";
+import { convertKeysToCamelCase } from "../utils/casingConverter";
+import { formatDate } from "../utils/formatDate";
 
 const initialState = [];
 
 const callerSlice = createSlice({
-	name: 'caller',
-	initialState,
-	reducers: {
-		addCaller(state, action) {
-			state.push(action.payload);
-			state[state.length - 1].callerType = 'stack';
-		},
-		removeCaller(state) {
-			state.shift();
-		},
-		addCallerToLookup(state, action) {
-			state.unshift(action.payload);
-			state[0].callerType = 'lookup';
-		},
-		removeBookingById(state, action) {
-			const { type, bookingId } = action.payload;
-			// Ensure we're targeting the correct list ('Current' or 'Previous')
-			if (state[0][type]) {
-				state[0][type] = state[0][type].filter(
-					(booking) => booking.bookingId !== bookingId
-				);
-			}
-		},
-	},
+  name: "caller",
+  initialState,
+  reducers: {
+    addCaller(state, action) {
+      state.push(action.payload);
+      state[state.length - 1].callerType = "stack";
+    },
+    removeCaller(state) {
+      state.shift();
+    },
+    addCallerToLookup(state, action) {
+      state.unshift(action.payload);
+      state[0].callerType = "lookup";
+    },
+    removeBookingById(state, action) {
+      const { type, bookingId } = action.payload;
+      // Ensure we're targeting the correct list ('Current' or 'Previous')
+      if (state[0][type]) {
+        state[0][type] = state[0][type].filter(
+          (booking) => booking.bookingId !== bookingId
+        );
+      }
+    },
+  },
 });
 
 export const addCallerToBooking = function (selectedRow, activeTab) {
-	return function (dispatch, getState) {
-		const type = activeTab === 'current-bookings' ? 'Current' : 'Previous';
-		let targetCallerData = getState().caller[0][type][selectedRow];
+  return function (dispatch, getState) {
+    const type = activeTab === "current-bookings" ? "Current" : "Previous";
+    let targetCallerData = getState().caller[0][type][selectedRow];
 
-		targetCallerData = {
-			...targetCallerData,
-			bookingType: type,
-			PickupDateTime:
-				type === 'Current' ? targetCallerData.PickupDateTime : new Date(),
-			// Add any additional properties you want to extend here
-		};
+    targetCallerData = {
+      ...targetCallerData,
+      bookingType: type,
+      PickupDateTime:
+        type === "Current" ? targetCallerData.PickupDateTime : new Date(),
+      scope: 0,
+      accountNumber: 9999,
+      // Add any additional properties you want to extend here
+    };
 
-		dispatch({ type: 'bookingForm/addData', payload: targetCallerData });
-		dispatch({ type: 'caller/removeCaller' });
-	};
+    dispatch({ type: "bookingForm/addData", payload: targetCallerData });
+    dispatch({ type: "caller/removeCaller" });
+  };
 };
 
 export const updateCurrentBookingWithLookup = function (
-	selectedRow,
-	activeTab
+  selectedRow,
+  activeTab
 ) {
-	return function (dispatch, getState) {
-		const type = activeTab === 'current-bookings' ? 'Current' : 'Previous';
-		let targetCallerData = convertKeysToCamelCase(
-			getState().caller[0][type][selectedRow]
-		);
-		const currentBooking =
-			getState().bookingForm.bookings[
-				getState().bookingForm.activeBookingIndex
-			];
-		const updatedBooking = {
-			...currentBooking,
-			...targetCallerData,
-			recurrenceRule: '',
-			pickupDateTime: formatDate(new Date()),
-		};
-		dispatch({
-			type: 'bookingForm/updateBookingData',
-			payload: updatedBooking,
-		});
-		dispatch({ type: 'caller/removeCaller' });
-	};
+  return function (dispatch, getState) {
+    const type = activeTab === "current-bookings" ? "Current" : "Previous";
+    let targetCallerData = convertKeysToCamelCase(
+      getState().caller[0][type][selectedRow]
+    );
+    const currentBooking =
+      getState().bookingForm.bookings[
+        getState().bookingForm.activeBookingIndex
+      ];
+    const updatedBooking = {
+      ...currentBooking,
+      ...targetCallerData,
+      recurrenceRule: "",
+      pickupDateTime: formatDate(new Date()),
+      scope: 0,
+      accountNumber: 9999,
+    };
+    dispatch({
+      type: "bookingForm/updateBookingData",
+      payload: updatedBooking,
+    });
+    dispatch({ type: "caller/removeCaller" });
+  };
 };
 
 export const { addCaller, removeCaller, addCallerToLookup, removeBookingById } =
-	callerSlice.actions;
+  callerSlice.actions;
 
 export default callerSlice.reducer;
