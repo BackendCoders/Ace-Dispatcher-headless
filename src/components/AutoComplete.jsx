12dbe5528b78b5f9d04cce2ce2bf1a2c1c172b6e
BackendCoders <teambackendcoders@gmail.com>
2026-02-07 12:59:38 +0530

    adding google apis for auto complete

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -44,7 +44,7 @@ const Autocomplete = ({
 						// address: item.formatted_address.join(' '),
 						postcode: item.postcode,
 						...item,
-					}))
+					})),
 				);
 				setShowOptions(true);
 			} catch (error) {
@@ -68,16 +68,16 @@ const Autocomplete = ({
 							address: filteredAddress,
 							raw: item, // Retain the original address for reference if needed
 						};
-					})
+					}),
 				);
 				setShowOptions(true);
 			}
 		}
-		if (type === 'postal') {
-			getPostalID();
-		} else {
-			fetchPoi();
-		}
+		// if (type === 'postal') {
+		// 	getPostalID();
+		// } else {
+		// 	fetchPoi();
+		// }
 	}, [inputValue]);
 
 	useEffect(() => {
@@ -117,7 +117,7 @@ const Autocomplete = ({
 		} else if (e.key === 'ArrowDown') {
 			e.preventDefault();
 			setActiveOptionIndex((prevIndex) =>
-				Math.min(prevIndex + 1, options.length - 1)
+				Math.min(prevIndex + 1, options.length - 1),
 			);
 		} else if (e.key === 'ArrowUp') {
 			e.preventDefault();
